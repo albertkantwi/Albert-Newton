d3296ede6915553a1e1c0bf5b13b71934e1508e6
albert antwi <albertkantwi@gmail.com>
2023-01-28 17:00:23 +0800

    apples and oaranges code challenge

--- trials.js
+++ trials.js
@@ -1,7 +1,33 @@
 "use strict";
 
 // Albert's solution
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    
+    let appleCount = [];
+    let orangeCount = [];
+    let appleAtSamHouse = 0;
+    let orangeAtSamHouse = 0;
+    apples.map((x) => {
+        appleCount.push(x + a);
+    })
+    oranges.map((x) => {
+        orangeCount.push(x + b);
+    })
+    appleCount.map((x) => {
+        if(x >=s && x <= t){
+            appleAtSamHouse++;
+        }
+    })
+    orangeCount.map((x) => {
+        if(x >=s && x <= t){
+            orangeAtSamHouse++;
+        }
+    })
+    console.log(appleAtSamHouse);
+    console.log(orangeAtSamHouse);
 
+
+}
 
 
 // Newton's solution
